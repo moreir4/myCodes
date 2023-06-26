39c478420f14318c08660a42585364b22f730e16
Matheus Moreira <matheusmsmg2@gmail.com>
2023-06-26 20:44:12 -0300

    1.0

---
+++ python/my_studies/ex010.py
@@ -0,0 +1,6 @@
+#Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode
+#comprar. Considere US$1,00 = R$3,27
+
+dinheiro = int(input('Informe quanto de dinheiro você tem na carteira: '))
+dolar = dinheiro / 3.27
+print(f'Com R${dinheiro}, você consegue comprar {dolar:.2f}US$')
