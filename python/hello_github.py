20fdfacff79c930bc7498164b9c10970e410a3d2
Matheus Moreira <matheusmsmg2@gmail.com>
2023-06-26 18:14:47 -0300

    modified:   python/hello_github.py

--- python/hello_github.py
+++ python/hello_github.py
@@ -1,1 +1,2 @@
 print('Olá, estou tentando publicar meus códigos no github. \nSe você esta lendo isso, é porque consegui! :)')
+print('Estou inserindo um novo texto par testar a atualização no github.')
