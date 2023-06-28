ad4055d9b2ced8e5e7458232bedbf47f79718519
Matheus Moreira <matheusmsmg2@gmail.com>
2023-06-28 20:58:01 -0300

    Exercicio 14 - Curso em Vídeo

---
+++ python/my_studies/ex014.py
@@ -0,0 +1,5 @@
+#Escreva um programa que converta uma temperatura digitada em ºC e converta para ºF
+
+c = float(input('Informe a temperatura em celsius: '))
+f = (c * 1.8) + 32
+print(f'{c}ºC = {f}ºF')
