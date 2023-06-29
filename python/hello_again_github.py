3d435d43f02502ebbc594611cd916d454717fa17
Matheus Moreira <matheusmsmg2@gmail.com>
2023-06-28 22:45:38 -0300

    directory relocation

--- python/hello_again_github.py
+++
@@ -1,1 +0,0 @@
-print('Meu segundo teste enviando um código.')
--- python/hello_github.py
+++
@@ -1,2 +0,0 @@
-print('Olá, estou tentando publicar meus códigos no github. \nSe você esta lendo isso, é porque consegui! :)')
-print('Estou inserindo um novo texto par testar a atualização no github.')
